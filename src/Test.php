a29ca5b3e469514dbc60bc0ff03a48653f5cd541
hezhenliang <hezhenliang@100tal.com>
2019-07-19 19:16:38 +0800

    update some file

--- src/Test.php
+++ src/Test.php
@@ -8,3 +8,10 @@ class Test
         return 'hello';
     }
 }
+
+//include "Helper.php";
+//$obj = new Helper();
+//
+//echo "<pre>";
+//$arr = ["int" => 123, 'a1' => ['a' => 1, 'b' => 2], 'a0' => ['a' => 0, 'b' => 3]];
+//var_dump($obj->array_sort($arr, 'a', 'asc'));
